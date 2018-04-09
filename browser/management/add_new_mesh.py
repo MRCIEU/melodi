2ce28f094c0f8d4bc4114c08e619d89163bab758
Ben Elsworth <be15516@it017374.users.bris.ac.uk>
2018-04-09 12:53:22 +0100

    tidying up mesh

---
+++ browser/management/add_new_mesh.py
@@ -0,0 +1,8 @@
+from mrcoc_parser import *
+
+#steps
+# 1. Get data for existing mesh-pmid
+# 2. Parse MRCOC data (mrcoc_parser.py)
+# 3. Add new mesh terms
+# 4. Add new mesh-pmid realationships
+# 5. Update mesh hierarchy
